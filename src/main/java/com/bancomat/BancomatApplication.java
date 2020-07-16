020ce06db6f20aed6dec8a6449343558e0b98c42
DragosPredi <predi.dragos@gmail.com>
2020-07-16 15:11:37 +0300

    Initial Commit - Setting up Spring Boot project

---
+++ src/main/java/com/bancomat/BancomatApplication.java
@@ -0,0 +1,13 @@
+package com.bancomat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancomatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BancomatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bancomat/BancomatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancomat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancomatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
