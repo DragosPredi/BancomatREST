5c9bdf669ee1a47180fb9b3648414158d39078ef
DragosPredi <predi.dragos@gmail.com>
2020-07-26 17:22:43 +0300

    Reformatted the tests to fit the current state of app

--- src/test/java/com/bancomat/ATMTests.java
+++ src/test/java/com/bancomat/ATMTests.java
@@ -1,16 +1,16 @@
-/*
 package com.bancomat;
 
-import com.atm.backend.services.AtmServiceImpl;
+import com.atm.backend.services.AtmService;
 import com.atm.backend.bills.Bill;
+import com.atm.backend.services.AtmServiceImpl;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-
 public class ATMTests {
+
     public void setArray(int[] arr, int one, int five, int ten, int fifty, int oneH) {
         arr[0] = one;
         arr[1] = five;
@@ -19,62 +19,76 @@ public class ATMTests {
         arr[4] = oneH;
     }
 
-    public void assertBillsQuantityHelper(int[] baseArray, String errorMsg, AtmServiceImpl tester) {
+    public void assertBillsQuantityHelper(int[] baseArray, String errorMsg, AtmService tester) {
         int cont = 0;
         for (Bill.Type billType : Bill.Type.values()) {
-            assertEquals(baseArray[cont++], tester.getBillsQuantityByType(billType), errorMsg);
+            assertEquals(baseArray[cont++], tester.getOneTypeBillQuantity(billType), errorMsg);
         }
     }
 
     @Test
-    public void setATMStateChecker(){
+    public void initialATMStateChecker(){
+        AtmService tester = new AtmServiceImpl();
         int []bills = {100, 100, 100, 50, 50};
-        AtmServiceImpl tester = AtmServiceImpl.getInstance();
-        tester.setATMState(100, 100, 100, 50, 50);
+        assertBillsQuantityHelper(bills, "set ATM state faulty", tester);
+    }
+
+    @Test
+    public void setATMStateChecker(){
+        AtmService tester = new AtmServiceImpl();
+
+        int []bills = {85, 0, 75, 80, 10};
+        tester.setATMState(bills);
         assertBillsQuantityHelper(bills, "set ATM state faulty", tester);
     }
 
     @Test
     public void fillUpChecker() {
-        AtmServiceImpl tester = AtmServiceImpl.getInstance();
-        tester.setATMState(100,100,100,50,50);
+        AtmService tester = new AtmServiceImpl();
 
-        tester.fillUp(Bill.Type.ONE_RON, 10);
-        tester.fillUp(Bill.Type.FIVE_RON, 10);
-        tester.fillUp(Bill.Type.TEN_RON, 10);
-        tester.fillUp(Bill.Type.FIFTY_RON, 10);
-        tester.fillUp(Bill.Type.ONEHUNDRED_RON, 10);
+        int []bills = {100, 100, 100, 50, 50};
 
-        int[] bills = {110, 110, 110, 60, 60};
+        tester.setATMState(bills);
+
+        tester.fillUpOneTypeBill(Bill.Type.ONE_RON, 10);
+        tester.fillUpOneTypeBill(Bill.Type.FIVE_RON, 10);
+        tester.fillUpOneTypeBill(Bill.Type.TEN_RON, 10);
+        tester.fillUpOneTypeBill(Bill.Type.FIFTY_RON, 10);
+        tester.fillUpOneTypeBill(Bill.Type.ONEHUNDRED_RON, 10);
+
+        setArray(bills, 110, 110, 110, 60,60);
         assertBillsQuantityHelper(bills, "Fill up is faulty", tester);
     }
 
     @Test
     public void modifyNrBillsChecker() {
-        AtmServiceImpl tester = AtmServiceImpl.getInstance();
-        tester.setATMState(100,100,100,50,50);
+        AtmService tester = new AtmServiceImpl();
 
-        int[] bills = {5, 1, 2, 3, 4};
+        int []bills = {100, 100, 100, 50, 50};
 
-        tester.modifyAllBills(bills, 1);
+        tester.setATMState(bills);
+
+        setArray(bills, 5, 1, 2,3, 4);
+
+        tester.addBills(bills);
 
         setArray(bills, 105, 101, 102, 53, 54);
-        assertBillsQuantityHelper(bills, "Modify all bills is faulty", tester);
+        assertBillsQuantityHelper(bills, "Add bills is faulty", tester);
 
         setArray(bills, 5, 1, 2, 3, 4);
-        tester.modifyAllBills(bills, -1);
+        tester.removeBills(bills);
 
         setArray(bills, 100, 100, 100, 50, 50);
-        assertBillsQuantityHelper(bills, "Initial state is faulty", tester);
+        assertBillsQuantityHelper(bills, "Remove bills is faulty", tester);
 
     }
 
     @Test
     public void withdrawalRequestsChecker() {
-        AtmServiceImpl tester = AtmServiceImpl.getInstance();
-        tester.setATMState(100,100,100,50,50);
+        AtmService tester = new AtmServiceImpl();
 
-        int[] bills = {1, 0, 0, 1, 1};
+        int []bills = {1, 0, 0, 1, 1};
+
         assertArrayEquals(bills, tester.withdrawalRequestAsArray(151));
 
         setArray(bills, 3, 0, 0, 0, 1);
@@ -83,18 +97,18 @@ public class ATMTests {
         setArray(bills, 4, 1, 4, 1, 0);
         assertArrayEquals(bills, tester.withdrawalRequestAsArray(99));
 
-        tester.setATMState(100, 100, 100, 50, 50);
+        setArray(bills, 100, 100, 100, 50, 50);
+        tester.setATMState(bills);
         setArray(bills, 0, 0, 1, 50, 50);
         assertArrayEquals(bills, tester.withdrawalRequestAsArray(7510));
     }
 
     @Test
     public void multipleWithdrawalRequestsChecker() {
-        AtmServiceImpl tester = AtmServiceImpl.getInstance();
-        tester.setATMState(100,100,100,50,50);
+        AtmService tester = new AtmServiceImpl();
 
-        int[] bills = new int[5];
-        tester.setATMState(10, 9, 5, 1, 1);
+        int[] bills = {10, 9, 5, 1, 1};
+        tester.setATMState(bills);
 
         setArray(bills, 0, 0, 0, 1, 1);
         assertArrayEquals(bills, tester.withdrawalRequestAsArray(150));
@@ -105,12 +119,11 @@ public class ATMTests {
         setArray(bills, 5, 9, 0, 0, 0);
         assertArrayEquals(bills, tester.withdrawalRequestAsArray(50));
 
-
-        tester.setATMState(10, 9, 5, 3, 10);
+        setArray(bills, 10, 9, 5, 3, 10);
+        tester.setATMState(bills);
 
         setArray(bills, 3, 1, 1, 1, 9);
         assertArrayEquals(bills, tester.withdrawalRequestAsArray(968));
     }
 
 }
-*/
