6b6c02b63f5efbdbedff59b094873b0eaf138501
DragosPredi <predi.dragos@gmail.com>
2020-07-26 17:21:27 +0300

    Added Swagger for API documentation

--- src/main/java/com/atm/backend/BancomatApplication.java
+++ src/main/java/com/atm/backend/BancomatApplication.java
@@ -3,13 +3,25 @@ package com.atm.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableSwagger2
 public class BancomatApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BancomatApplication.class, args);
+    }
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.atm.backend")).build();
     }
 
 }
