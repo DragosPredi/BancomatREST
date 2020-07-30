faac4896a1437d7a3bc11213a2990213dbf3be07
DragosPredi <predi.dragos@gmail.com>
2020-07-30 09:35:42 +0300

    Further refactoring of functions

--- src/main/java/com/atm/backend/services/CashRequestServiceImpl.java
+++ src/main/java/com/atm/backend/services/CashRequestServiceImpl.java
@@ -8,36 +8,50 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class CashRequestServiceImpl implements CashRequestService {
 
-    private final AtmService Atm;
+    private final AtmService localAtm;
     private final RemoteAtmService remoteAtm;
 
     public CashRequestServiceImpl(AtmService atm, RemoteAtmService remoteAtm) {
-        Atm = atm;
+        this.localAtm = atm;
         this.remoteAtm = remoteAtm;
+    }
+
+    public Map<String, Integer> addMapValues(Map<String, Integer> map1, Map<String, Integer> map2){
+        map1.replaceAll((s, v) -> map1.get(s) + map2.get(s));
+        return map1;
+    }
+
+    public SoldInquiryDto requestRemoteCashDifference(int totalAmount){
+        int totalAmountAvailableLocally = localAtm.totalAmountAvailable();
+        return remoteAtm.remoteWithdrawalRequest(totalAmount - totalAmountAvailableLocally);
+    }
+
+    public ResponseEntity<SoldInquiryDto> computeRemoteTransactionIfPossible(int amountRequired){
+        HashMap<Bill.Type, Integer> availableInLocalAtm = localAtm.withdrawAllMoney();
+        SoldInquiryDto body = requestRemoteCashDifference(amountRequired);
+
+        if (body != null) {
+            HashMap<String, Integer> localBills = MyUtils.billTypeToStringTypeMapConverter(availableInLocalAtm);
+            body.setBills(addMapValues(body.getBills(), localBills));
+            return new ResponseEntity<>(body, HttpStatus.OK);
+        } else {
+            localAtm.fillUpWithMap(availableInLocalAtm);
+            return null;
+        }
     }
 
     @Override
     public ResponseEntity<SoldInquiryDto> withdrawalRequest(int cashAmount) {
-        SoldInquiryDto body = Atm.withdrawalRequest(cashAmount);
+        SoldInquiryDto body = localAtm.localWithdrawalRequest(cashAmount);
         if (body != null) {
             return new ResponseEntity<>(body, HttpStatus.OK);
         } else {
-            int totalAmountAvailableLocally = Atm.totalAmountAvailable();
-            HashMap<Bill.Type, Integer> availableInLocalAtm = Atm.withdrawAllMoney();
-            body = remoteAtm.remoteWithdrawalRequest(cashAmount - totalAmountAvailableLocally);
-            if (body != null) {
-                HashMap<String, Integer> localBills = MyUtils.billTypeToStringTypeMapConverter(availableInLocalAtm);
-                for (String st : body.getBills().keySet())
-                    body.getBills().put(st, body.getBills().get(st) + localBills.get(st));
-                return new ResponseEntity<>(body, HttpStatus.OK);
-            } else {
-                Atm.fillUpWithMap(availableInLocalAtm);
-                return null;
-            }
+            return computeRemoteTransactionIfPossible(cashAmount);
         }
     }
 }
